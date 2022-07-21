fb572fc5fa4628223d915f4e094bc98fe4eb8b5a
prajwalraut199 <84006806+prajwalraut199@users.noreply.github.com>
2022-07-21 23:10:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,169 +1,105 @@
-  #include<iostream>
-
+#include<iostream>
 #include<conio.h>
-
 #include<windows.h>
 
 using namespace std;
 
 class Bank{
-
 	private: 
-
 	    int total_;
-
 	    string id;
-
+	
 	    struct user{
-
 	    	string name,ID,address;
-
 	    	int contact,cash;
-
 		}user[100];
 
 	public:
 
 		Bank(){
-
-			total_=0;
-
+		    total_=0;
 		}
-
 		void choice();
-
 		void perData();
-
 		void show();
-
 		void update();
-
 		void search();
-
 		void transactions();
-
 		void del();
-
 };
-
 int main(){
 
 	Bank b;
-
 	b.choice();
-
 	return 0;
-
 }
 
 void Bank::choice(){
 
 	char ch;
-
 	while(1){
-
 	cout<<"\n\nPRESS..!!"<<endl;
-
 	cout<<"1. Create new account"<<endl;
-
 	cout<<"2. View customers list"<<endl;
-
 	cout<<"3. Update information of existing account"<<endl;
-
 	cout<<"4. Check the details of an existing account"<<endl;
-
 	cout<<"5. For transactions"<<endl;
-
 	cout<<"6. Remove existing account"<<endl;
-
 	cout<<"7. Exit"<<endl;
-
 	ch=getch();
 
 	system("CLS");
-
+		
 	switch(ch){
-
 		case '1':
-
 			Bank::perData();
-
 			break;
 
 		case '2':
-
 			if(total_==0){
-
 				cout<<"No data is entered"<<endl;
-
-			}else{
-
+			}
+			else{
 			Bank::show();
-
 		   }
 
 			break;
 
 		case '3':
-
 			if(total_==0)
-
 			cout<<"No data is entered"<<endl;
-
 			else
-
 			Bank::update();
-
 			break;
 
 		case '4':
-
 			if(total_==0)
-
 			cout<<"No data is entered"<<endl;
-
 			else
-
 			Bank::search();
-
 			break;
 
 		case '5':
-
 			if(total_==0)
-
 			cout<<"No data is entered"<<endl;
-
 			else
-
 			Bank::transactions();
-
 			break;
-
+			
 		case '6':
-
 			if(total_==0)
-
 			cout<<"No data is entered"<<endl;
-
 			else
-
 			Bank::del();
-
 			break;
 
 		case '7':
-
 			exit(0);
-
 			break;
-
 		default:
-
 			cout<<"Invalid input"<<endl;
 
 			break;
-
 	}
 
     }
@@ -172,16 +108,17 @@ void Bank::choice(){
 
 void Bank::perData(){
 
-	cout<<"Enter data of user "<<total_+1<<endl;
-
-	cout<<"Enter name: ";
-
+	
+	cout<<"Enter data "<<total_+1<<endl;
+	
+	cout<<"Enter name of user ";
+	
 	cin>>user[total_].name;
-
+	
 	cout<<"ID: ";
-
+	
 	cin>>user[total_].ID;
-
+	
 	cout<<"Address: ";
 
 	cin>>user[total_].address;
